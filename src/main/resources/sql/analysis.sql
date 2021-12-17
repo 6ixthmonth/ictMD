d00e5fd1fe73ac473af73e7af654f89bb6717739
June <6ixthmonth@gmail.com>
2021-12-17 11:51:20 +0900

    Remove classpath in mybatis-config.xml

--- src/main/resources/sql/analysis.sql
+++ src/main/resources/sql/analysis.sql
@@ -1,13 +1,17 @@
-DROP TABLE analysis;
-DROP SEQUENCE anaSeq;
-
-CREATE TABLE analysis (
+create table analysis (
       num NUMBER
     , tags VARCHAR2(100)
     , elements VARCHAR2(100)
-    , searchDate DATE
+    , searchDate DATE default sysdate
     , make VARCHAR2(100)
     , model VARCHAR2(100)
 );
 
-CREATE SEQUENCE anaSeq;
+create sequence anaSeq;
+
+create table history (
+    id varchar2(100) not null,
+    views number default 1,
+    favorites number default 0,
+    comments number default 0
+);
--- src/main/resources/sql/model.sql
+++ src/main/resources/sql/model.sql
@@ -24,21 +24,3 @@ create table modelDetail (
     weight varchar2(100),
     etc varchar2(100)
 );
-
-create table analysis (
-      num NUMBER
-    , tags VARCHAR2(100)
-    , elements VARCHAR2(100)
-    , searchDate DATE default sysdate
-    , make VARCHAR2(100)
-    , model VARCHAR2(100)
-);
-
-create sequence anaSeq;
-
-create table history (
-    id varchar2(100) not null,
-    views number default 1,
-    favorites number default 0,
-    comments number default 0
-);
