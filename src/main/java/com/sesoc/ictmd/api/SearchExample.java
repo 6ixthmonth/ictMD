2d091ea954d74d58a5c59ad88d4ca4208d3e3d56
june0326 <6ixthmonth@gmail.com>
2022-03-03 17:53:05 +0900

    Edit search example

--- src/main/java/com/sesoc/ictmd/api/SearchExample.java
+++ src/main/java/com/sesoc/ictmd/api/SearchExample.java
@@ -1,37 +1,52 @@
 package com.sesoc.ictmd.api;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.photos.Photo;
+import com.flickr4java.flickr.photos.PhotoList;
+import com.flickr4java.flickr.photos.PhotosInterface;
+import com.flickr4java.flickr.photos.SearchParameters;
 
 @Component
 @PropertySource("classpath:key.properties")
 public class SearchExample {
 
 	@Value("${FLICKR_API_KEY}")
-	String apiKey;
+	private String apiKey;
 
 	@Value("${FLICKR_SHARED_SECRET}")
-	String sharedSecret;
+	private String sharedSecret;
 
 	Flickr f;
 
 	REST rest;
 
-	public SearchExample() {
-		System.out.println("flickr api key at constructor: " + apiKey);
-		System.out.println("flickr shared secret at constructor: " + sharedSecret);
+	public void initFlickr() {
+		f = new Flickr(this.apiKey, this.sharedSecret, new REST());
 	}
-	
-	public String getApiKey() {
-		return apiKey;
-	}
-	
-	public String getSharedSecret() {
-		return sharedSecret;
+
+	private void search(String text) throws FlickrException {
+		PhotosInterface photos = f.getPhotosInterface();
+		SearchParameters params = new SearchParameters();
+		params.setMedia("videos"); // One of "photos", "videos" or "all"
+		params.setExtras(Stream.of("media").collect(Collectors.toSet()));
+		params.setText(text);
+		PhotoList<Photo> results = photos.search(params, 5, 0);
+
+		results.forEach(p -> {
+			System.out.println(String.format("Title: %s", p.getTitle()));
+			System.out.println(String.format("Media: %s", p.getMedia()));
+			System.out.println(String.format("Original Video URL: %s", p.getVideoOriginalUrl()));
+		});
+
 	}
 
 }
